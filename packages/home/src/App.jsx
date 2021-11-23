df81f1b7eb0a8799fb3f86ca3971a7954341d22f
Özkan Onay <ozkanonay@gmail.com>
2021-11-23 18:09:05 +0300

    state sharing between apps

--- packages/home/src/App.jsx
+++ packages/home/src/App.jsx
@@ -12,12 +12,21 @@ const Header = wrapComponent(React.lazy(() => import("nav/Header")));
 
 import "./index.css";
 
-const App = () => (
-    <div>
-        <Header />            
-        <div>Hi there, I'm React from Webpack 5.</div>
-    </div>
-);
+const App = () => {
+    const [itemCount, itemCountSet] = React.useState(0);
+    const onAddToCart = () => {
+        itemCountSet(itemCount + 1);
+    }
+
+    return (
+        <div>
+            <Header count={itemCount} onClear={() => itemCountSet(0)} />            
+            <div>Hi there, I'm React from Webpack 5.</div>
+            <button onClick={onAddToCart}>Buy me</button>
+            <div>Cart count is {itemCount}</div>
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
--- packages/nav/src/index.js
+++ packages/nav/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Header = () => (
-    <header style={{ fontSize: "xx-large" }}>Header!!</header>
-);
+const Header = ({count, onClear}) => {
+    
+    return (
+        <header style={{ fontSize: "xx-large" }}>
+            
+            <span>Header - Cart count is {count}</span>
+            <button onClick={onClear}>Clear</button>
+        </header>
+    );
+};
 
 export default Header;
